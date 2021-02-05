019bc6fd111e05a0f75a973bb34ea6fd61194837
gie-fortuna <GFortuna@qa.com>
2021-02-05 14:28:18 +0000

    added update

---
+++ API/createCar.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const thisName=document.querySelector("#name").value
+const thisMake=document.querySelector("#make").Value
+const thisModel=document.querySelector("#model").value
+
+
+let data={
+    name:thisName,
+    make: thisMake,
+    model: thisModel
+}
+
+const createCar=(field)=>{
+
+}
