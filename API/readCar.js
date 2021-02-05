13f86aa58075f733828d183c2cbdbef242d19444
gie-fortuna <GFortuna@qa.com>
2021-02-05 13:23:42 +0000

    Added read all but not functional yet

--- API/readCar.js
+++ API/readCar.js
@@ -1,27 +1,37 @@
 'use strict';
 
 const carToDisplay = document.querySelector("#displayBox")
+const allCarsToDisplay = document.querySelector("#displayBoxAll")
 const givenId = document.querySelector("#carId")
-const carBlock = document.querySelector("#carPara")
 
-const printToScreen = (car) => {
+const printToScreen =(car) => {
     for (let info in car) {
-        carToDisplay.append("\n");
         let actualText = document.createTextNode(
-            `${info}: ${car[info]} `);
-        console.log(info);
-        console.log(car[info]);
-        carBlock.append(actualText)
-        carToDisplay.appendChild(carBlock)
+            `${info}: ${car[info]} `
+            );
+        console.log(info+":" +car[info]);
+        carToDisplay.append(actualText)
     }
    
+}
+
+const printToScreenAll=(cars)=>{
+    for (let car in cars){
+        for (let info in cars){
+            let actualText = document.createTextNode(
+                `${info}: ${car[info]} `
+                );
+            console.log(info+":" +car[info]);
+            allCarsToDisplay.append(actualText)
+        }
+    }
 }
 
 
 
 
 const readCarById = () => {
-    carBlock.innerHTML = "";
+    carToDisplay.innerHTML="";
     const idVal = givenId.value;
     fetch("http://localhost:9092/car/read/" + idVal)
         .then((res) => {
@@ -32,6 +42,23 @@ const readCarById = () => {
                 .then((data) => {
                     console.log(data);
                     printToScreen(data)
+                }).catch((err) => console.log(err))
+        })
+}
+
+const readAllCars = () => {
+    allCarsToDisplay.innerHTML = "";
+    fetch("http://localhost:9092/car/read/")
+        .then((res) => {
+            if (res.ok != true) {
+                console.log("Status is not ok!");
+            }
+            res.json()
+                .then((data) => {
+                    console.log(data);
+                    for (let car in data){
+                        printToScreenAll(car)
+                    }
                 }).catch((err) => console.log(err))
         })
 }
