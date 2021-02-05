c2c103311c4e27d2a34182894a62dc23b8f6cc21
gie-fortuna <GFortuna@qa.com>
2021-02-05 12:36:55 +0000

    Read is working

--- API/readCar.js
+++ API/readCar.js
@@ -1,26 +1,37 @@
 'use strict';
-const carToDisplay=document.querySelector("#displayBox")
-const givenId=document.getElementById("#carId")
 
-const printToScreen=(car)=>{
-    for (let info of car){
-        let actualText=document.createTextNode(info);
-        carToDisplay.append(actualText);
+const carToDisplay = document.querySelector("#displayBox")
+const givenId = document.querySelector("#carId")
+const carBlock = document.querySelector("#carPara")
+
+const printToScreen = (car) => {
+    for (let info in car) {
+        carToDisplay.append("\n");
+        let actualText = document.createTextNode(
+            `${info}: ${car[info]}, `);
+        console.log(info);
+        console.log(car[info]);
+        carBlock.append(actualText)
+        carToDisplay.appendChild(carBlock)
     }
+   
 }
-    
 
 
 
-const readCarById=()=>{
-    carToDisplay.innerHTML="";
-    fetch("http://localhost:9092/car/read/"+givenId)
-    .then((res)=>{ 
-        if(res.ok!=true){
-            console.log("Status is not ok!");
-        }res.json()
-    }).then((data)=>{
-        console.log(data);
-        printToScreen(data)
-    }).catch((err)=>console.log(err))
+
+const readCarById = () => {
+    carBlock.innerHTML = "";
+    const idVal = givenId.value;
+    fetch("http://localhost:9092/car/read/" + idVal)
+        .then((res) => {
+            if (res.ok != true) {
+                console.log("Status is not ok!");
+            }
+            res.json()
+                .then((data) => {
+                    console.log(data);
+                    printToScreen(data)
+                }).catch((err) => console.log(err))
+        })
 }
